fa4c037d1ef8e289041c4b4d63adc68bfaf586e6
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-05 11:53:13 +0000

    added class functionality to map items

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,3 +1,12 @@
 class Todo
-    
+    def self.all
+        if ENV['ENVIRONMENT'] == 'todo_test'
+            connection = PG.connect(dbname: 'todo_test')
+        else
+            connection = PG.connect(dbname: 'to-do-list_database')
+        end
+
+        result = connection.exec("SELECT * FROM todolist_table")
+        result.map { |todo| todo['todo_item'] }
+    end
 end
